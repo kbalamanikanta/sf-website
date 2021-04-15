a971e28f4cb1af86c811cde04b7eaa040d84ce01
Bala Manikanta Koorakula <bala@baygrape.in>
2021-04-15 17:44:02 +0530

    new change

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,13 +1,12 @@
 var express = require("express");
 var router = express.Router();
-var sendpulse = require("sendpulse-api");
 
 router.get("/welcome",function(req,res,next){
    res.send("Welcome to Slogfox");
 });
 
 router.get('/contact', (req, res) => {
-  
+   res.send("Sorry! Technical Problem. Please email to contact@slogfox.com");
 });
 
 module.exports = router;
